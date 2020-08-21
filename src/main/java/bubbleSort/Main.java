1560293c13feda499c1ac0446147163eb6571c24
Bekturs-n <beka.kadyrkulov.1@gmail.com>
2020-08-21 21:09:53 +0300

    bubble sorting

--- src/main/java/bubbleSort/Main.java
+++ src/main/java/bubbleSort/Main.java
@@ -11,14 +11,14 @@ import java.util.Arrays;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Сортировка пузырьком первый вариант:");
+        System.out.println("Сортировка пузырьком первый вариант");
         System.out.println("Входные данные: " + Arrays.toString(new int[]{5, 3, 9, 1, 2, 7}));
-        System.out.println("Итог:");
+        System.out.print("Итог: ");
         System.out.println(Arrays.toString(BubbleSort_v1.sort(new int[]{5, 3, 9, 1, 2, 7})));
         System.out.println("===============================================================");
-        System.out.println("Сортировка пузырьком второй вариант:");
+        System.out.println("Сортировка пузырьком второй вариант");
         System.out.println("Входные данные: " + Arrays.toString(new int[]{8, 3, 9, 5, 2, 7}));
-        System.out.println("Итог:");
+        System.out.print("Итог: ");
         System.out.println(Arrays.toString(BubbleSort_v2.sort(new int[]{8, 3, 9, 5, 2, 7})));
     }
 }
